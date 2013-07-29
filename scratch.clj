6e1ca626791d77d48bb74411da708cc337b93c96
Craig Andera <candera@wangdera.com>
2013-07-29 03:51:19 +0000

    Simple PWM control logic.

--- scratch.clj
+++ scratch.clj
@@ -1,6 +1,5 @@
-(import '[com.pi4j.io.gpio GpioFactory PinState RaspiPin])
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(def gpio (GpioFactory/getInstance))
 (def pin (.provisionDigitalOutputPin gpio
                                      RaspiPin/GPIO_17
                                      "MyLED"
@@ -17,3 +16,46 @@ pin
 (.toggle pin)
 (def f (.blink pin 250))
 (future-cancel f)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(def pwm (.provisionPwmOutputPin gpio
+                                 RaspiPin/GPIO_01 ; #18
+                                 "Motor Speed"
+                                 0      ; Default value
+                                 ))
+
+(def i1 (.provisionDigitalOutputPin gpio
+                                    RaspiPin/GPIO_02 ; #21/27
+                                    "I1"
+                                    PinState/LOW))
+
+(def i2 (.provisionDigitalOutputPin gpio
+                                    RaspiPin/GPIO_03 ; #22
+                                    "I2"
+                                    PinState/LOW))
+
+(def done (atom false))
+
+(def f
+  (let [done (atom false)]
+    (future
+      (loop [[x & more] (cycle (range 0 1024 100))]
+        (.setPwm pwm (first x))
+        (Thread/sleep 10)
+        (when (not done) (recur (rest x)))))
+    done))
+
+(reset! f true)
+
+(dotimes [_ 4]
+  (doseq [x (lazy-cat (range 0 1024 8) (range 1024 0 -8))]
+    (.setPwm pwm x)
+    (Thread/sleep 10)))
+
+(do (dotimes [_ 1000]
+       (.setPwm pwm 0)
+       (.setPwm pwm 1024))
+    (.setPwm pwm 512))
+
+(.setPwm pwm 1024)
