ea3d4f49d7a5544e93de271d46a0bfd5f520ccd3
Craig Andera <candera@wangdera.com>
2013-08-13 02:21:06 +0000

    More PWM scribblings.

--- scratch.clj
+++ scratch.clj
@@ -19,6 +19,9 @@ pin
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+;; Not sure this next line is necessary:
+(com.pi4j.wiringpi.Gpio/wiringPiSetup)
+
 (def pwm (.provisionPwmOutputPin gpio
                                  RaspiPin/GPIO_01 ; #18
                                  "Motor Speed"
@@ -26,12 +29,12 @@ pin
                                  ))
 
 (def i1 (.provisionDigitalOutputPin gpio
-                                    RaspiPin/GPIO_02 ; #21/27
+                                    RaspiPin/GPIO_00 ; #21/27
                                     "I1"
                                     PinState/LOW))
 
 (def i2 (.provisionDigitalOutputPin gpio
-                                    RaspiPin/GPIO_03 ; #22
+                                    RaspiPin/GPIO_07 ; #22
                                     "I2"
                                     PinState/LOW))
 
@@ -49,7 +52,7 @@ pin
 (reset! f true)
 
 (dotimes [_ 4]
-  (doseq [x (lazy-cat (range 0 1024 8) (range 1024 0 -8))]
+  (doseq [x (lazy-cat (range 600 1024 8) (range 1024 650 -8))]
     (.setPwm pwm x)
     (Thread/sleep 10)))
 
@@ -59,6 +62,83 @@ pin
     (.setPwm pwm 512))
 
 (.setPwm pwm 1024)
+
+(.setPwm pwm 0)
+(.setPwm pwm 250)
+(.setPwm pwm 800)
+
+(.getProperties pwm)
+(.getMode pwm)
+(.setMode pwm com.pi4j.io.gpio.PinMode/PWM_OUTPUT)
+
+(.setPwm pwm 100)
+
+(.high i1)
+(.low i2)
+(.high i2)
+
+(do
+  (.toggle i1)
+  (.toggle i2))
+
+(defn go [speed]
+  (if (< speed 0)
+    (do
+      (.low i1)
+      (.high i2))
+    (do
+      (.low i2)
+      (.high i1)))
+
+  (.setPwm pwm (-> (Math/abs speed) (/ 100.0) (* (- 1024 660)) (+ 660) long)))
+
+(defn stop []
+  (.low i1)
+  (.low i2))
+
+(-> 20 (/ 100.0) (* (- 1024 660)) (+ 660) long)
+
+(go 10)
+(stop)
+
+(.high i2)
+(.low i1)
+
+;; Wiring info:
+;; Brown = Black = Ground
+;; Orange = Red = Positive
+;; Yellow = White = Signal
+
+;;; Servo control
+
+(.setPwm pwm 1000)
+
+(dotimes [_ 1000]
+  (.setPwm pwm 15)
+  (Thread/sleep 1))
+
+(doseq [i (range 0 1024)]
+  (.setPwm pwm i)
+  (Thread/sleep 10))
+
+;; Software PWM
+
+(com.pi4j.wiringpi.Gpio/wiringPiSetup)
+
+(def i1 (.provisionDigitalOutputPin gpio
+                                    RaspiPin/GPIO_00 ; #21/27
+                                    "I1"
+                                    PinState/LOW))
+
+(def pwm (com.pi4j.wiringpi.SoftPwm/softPwmCreate 1 ; #21/27
+                                                  (int 100)
+                                                  (int 200)))
+
+(com.pi4j.wiringpi.SoftPwm/softPwmWrite 1 (int 20))
+
+(doseq [n (take 1000 (cycle (range 10 20 1)))]
+  (com.pi4j.wiringpi.SoftPwm/softPwmWrite 1 (int n))
+  (Thread/sleep 500))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
